aee5c8040878dde449097c95d0aa152aabd9776f
nebrassy <nebras30@gmail.com>
2021-06-08 18:18:29 +0200

    kill vendor_init

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,11 +16,6 @@ TARGET_OTA_ASSERT_DEVICE := alioth
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_alioth
-#TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
-TARGET_RECOVERY_DEVICE_MODULES := libinit_alioth
-
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/alioth
 TARGET_KERNEL_CONFIG := vendor/alioth_defconfig
