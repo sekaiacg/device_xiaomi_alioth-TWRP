def36887dac32b57be161e2a7072f416a636c38c
sekaiacg <sekaiacg@gmail.com>
2022-06-08 15:53:25 +0800

    device: Use miui_prebuilt instead of PRODUCT_COPY_FILES
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- device.mk
+++ device.mk
@@ -45,7 +45,4 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # PRODUCT_RELEASE_NAME ro.twrp.device.name
 PRODUCT_PROPERTY_OVERRIDES += ro.twrp.device.name=$(PRODUCT_RELEASE_NAME)
 
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/alioth/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/$(PRODUCT_RELEASE_NAME)/modules,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules) \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/$(PRODUCT_RELEASE_NAME)/firmware,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware)
+TWRP_REQUIRED_MODULES += miui_prebuilt
