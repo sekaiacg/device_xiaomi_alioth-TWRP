e80c7a7b8307a58498b31f775b6c3efa038d45d3
sekaiacg <sekaiacg@gmail.com>
2022-06-08 15:52:42 +0800

    device: Remove libandroidicu
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- device.mk
+++ device.mk
@@ -22,9 +22,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti.recovery \
     fastbootd
 
-PRODUCT_HOST_PACKAGES += \
-    libandroidicu
-
 # SHIPPING API
 PRODUCT_SHIPPING_API_LEVEL := 30
 # VNDK API
